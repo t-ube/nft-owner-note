678d8ac85903d7cdc12e67dc4bac37e0dbce719d
t-ube <ube@coder.okinawa.jp>
2025-01-19 14:00:09 +0900

    bugfix

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -1,6 +1,8 @@
 // app/projects/[projectId]/page.tsx
 import ProjectDetailWrapper from '@/app/components/ProjectDetailWrapper';
 
+export const runtime = 'edge';
+
 export default function ProjectDetailPage({ params }: { params: { projectId: string } }) {
   return <ProjectDetailWrapper projectId={params.projectId} />;
 }
