3f22ef6fceb77e11d5fd56c6be599d3e9142f900
t-ube <ube@coder.okinawa.jp>
2025-01-25 23:59:34 +0900

    add

--- src/app/components/ThemeToggle.tsx
+++ src/app/components/ThemeToggle.tsx
@@ -6,13 +6,13 @@ import { useTheme } from "next-themes"
 import { Button } from "@/components/ui/button"
 
 export function ThemeToggle() {
-  const { theme, setTheme } = useTheme()
+  const { resolvedTheme, setTheme } = useTheme()
 
   return (
     <Button
       variant="outline"
       size="icon"
-      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      onClick={() => setTheme(resolvedTheme === "light" ? "dark" : "light")}
       className="relative w-10 h-10"
     >
       <Sun className="absolute h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
